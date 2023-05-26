5f32e483ad67965b557caa131690402ab82747d0
RuiHirano <r.hrn.0930@gmail.com>
2023-05-26 19:11:26 +0900

    add custom command label name

--- src/auto_gpt_bard_plugin/settings.py
+++ src/auto_gpt_bard_plugin/settings.py
@@ -2,8 +2,16 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     BARD_SESSION_TOKEN: str
+    BARD_COMMAND_LABEL: str = "chat_with_bard"
+    BARD_COMMAND_NAME: str = "Chat with bard"
 
     class Config:
         case_sensitive = True
+        
+    @property
+    def __name__(self):
+        # here is trick to avoid getting this class as a plugin
+        # autogpt/plugins.py, line: 231, if ... and a_module.__name__ != "AutoGPTPluginTemplate"
+        return "AutoGPTPluginTemplate"
 
 settings = Settings()
